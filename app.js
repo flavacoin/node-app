70476bc0654a09cbbc2aa5f5dc6624853d85568a
flavacoin <79034517+flavacoin@users.noreply.github.com>
2023-02-09 04:06:38 +0000

    Update app.js
    text change

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello all the people!'))
+app.get('/', (req, res) => res.send('Hello all the people1!'))
 app.listen(3000, () => console.log('Server ready'))
